d566741d98b887c4c85122494d1595dafb98c1e9
myname1111 <animationstudiocp@gmail.com>
2024-06-21 21:29:55 +0700

    Add maze

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # https://youtu.be/qSBmjSsscI4?si=vqTnT000AKvSXsvN
 # Tutorial Pygames : https://youtu.be/AY9MnQ4x3zk?si=MllMZl8EfBFfR9KR
 from dataclasses import dataclass
-from typing import Optional, Tuple
+from typing import Optional, Self, Tuple
 
 import pygame
 
@@ -12,11 +12,26 @@ dir = "E:/Python/maze_game_test"
 
 @dataclass
 class Vec2:
-    x: int
-    y: int
+    x: float
+    y: float
 
-    def to_tuple(self) -> Tuple[int, int]:
+    def to_tuple(self) -> Tuple[float, float]:
         return (self.x, self.y)
+
+    def __add__(self, other: Self):
+        return Vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Self):
+        return Vec2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other: Self):
+        return Vec2(self.x * other.x, self.y * other.y)
+
+    def __truediv__(self, other: Self):
+        return Vec2(self.x / other.x, self.y / other.y)
+
+    def __floordiv__(self, other: Self):
+        return Vec2(self.x // other.x, self.y // other.y)
 
 
 class Player:
@@ -27,13 +42,52 @@ class Player:
 
     def on_key(self, key: int):
         if key == pygame.K_LEFT:
-            self.position.x += 10
-        elif key == pygame.K_RIGHT:
             self.position.x -= 10
+        elif key == pygame.K_RIGHT:
+            self.position.x += 10
 
     def render(self, screen: pygame.Surface):
         screen.blit(self.image, self.position.to_tuple())
 
+def get_points(pos: Vec2, size: Vec2) -> Tuple[Vec2, Vec2, Vec2, Vec2]:
+    return (
+        Vec2(pos.x, pos.y),
+        Vec2(pos.x, pos.y + size.y),
+        Vec2(pos.x + size.x, pos.y),
+        Vec2(pos.x + size.x, pos.y + size.y),
+    )
+
+@dataclass
+class Maze:
+    maze: list[bool]
+    cols: int
+    rows: int
+
+    def is_collide(self, row: int, col: int) -> bool:
+        return self.maze[row + self.rows * col]
+
+
+@dataclass
+class MazeSprite:
+    cell_size: Vec2
+    cell_image: pygame.Surface
+    maze: Maze
+    offset: Vec2
+
+    def to_index(self, pos: Vec2) -> Tuple[int, int]:
+        out = (pos - self.offset) // self.cell_size
+        return (int(out.x), int(out.y))
+
+    def is_point_collide(self, pos: Vec2) -> bool:
+        index = self.to_index(pos)
+        return self.maze.is_collide(index[0], index[1])
+
+    def collide_with_sprite(self, other_pos: Vec2, other_size: Vec2) -> bool:
+        points = get_points(other_pos, other_size)
+        is_collide = False
+        for point in points:
+            is_collide |= self.is_point_collide(point)
+        return is_collide
 
 pygame.init()
 window = pygame.display.set_mode((1280, 720))
@@ -50,3 +104,5 @@ while running:
 
     player.render(window)
     pygame.display.flip()
+
+    window.fill((0, 0, 0))
